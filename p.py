7a5d49935367f4cc276f4b38de3524ae73a56de2
rajattolani78-cyber <rajattolani78@gmail.com>
2025-10-11 23:25:35 +0530

    h

--- p.py
+++ p.py
@@ -1,14 +1,17 @@
-w = str(input())
+# it take input of word from the user :-]
+w = str(input("enter a word to count: "))
+# deploy a empty zero variable :-}
 vowel_count = 0
+# it reapeat the user input to check the variable 
 for r in (w):
-    if r =='a':
+    if r =='a':#it ckeck for the word "a" in the input and add +1 to the empty variable
         vowel_count+=1
-    elif r=='e':
+    elif r=='e':#it ckeck for the word "e" in the input and add +1 to the empty variable
         vowel_count+=1
-    elif r=='i':
+    elif r=='i':#it ckeck for the word "i" in the input and add +1 to the empty variable
         vowel_count+=1
-    elif r=='o':
+    elif r=='o':#it ckeck for the word "o" in the input and add +1 to the empty variable
         vowel_count+=1
-    elif r=='u':
+    elif r=='u':#it ckeck for the word "u" in the input and add +1 to the empty variable
         vowel_count+=1   
 print (vowel_count)
