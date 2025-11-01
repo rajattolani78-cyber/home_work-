aedbdd79090f44e47324b860e7cc2ba7f4f86245
rajattolani78-cyber <rajattolani78@gmail.com>
2025-11-01 09:32:27 +0530

    t

--- s.py
+++ s.py
@@ -1,14 +1,37 @@
-# r = 7 
-# for i in range (r):
-#     for j in range (r) :
-#        if i == j or j== r-1-i:
-#         print("*",end =" ")
-#        else:
-#          print(" ",end =" ")
-#     print()
-# reapeting variable both 
-name = "python"
-if name == ["rajat","java"]:
-    print("log in succesfull")
-else :
-    print("not valid")
+#-------------random die roll 
+# import random 
+# while True:
+#     i = input("if you want to play press 'enter', if you want to quit press 'q':  ")
+#     if i == 'q':
+#         print("Thank you!")
+#         break
+#     elif i == '':
+#         print(random.randint(1,6))
+#-------------word counter 
+# random_words = ["india3
+# ","idonasia","japan","inderpresh","sunio","prestige","procetion","iodine","iron"]
+# cont = 0
+# ra = []
+# for i in random_words:
+#     if i[0]=="j":
+#         cont = cont + 1
+#         ra.append(i)
+# print(cont,ra)
+# secreat_number = random.randint(1,90)
+# attampts = 10
+
+# while attampts >= 1:
+#     user_input = int(input("a guess number between (1 to 6): "))
+#     if user_input == secreat_number:
+#         print("you made it")
+#         break
+#     else:
+#         print (f"you have {attampts} left")
+#         if user_input < secreat_number:
+#             print ("try a higher number")
+#         else:
+#             print("try lower number")
+#     attampts-=1
+# print (f"GAME OVER,the seacreat number was {secreat_number}")
+    
+ 
